01de9f7c78bd59f6b4dd62293e855de2a4c3749c
Hector PHARAM <hector.pharam@se.com>
2023-03-15 11:11:34 +0100

    nvim:Added snip pluging

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,6 +20,12 @@ local plugins = {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
+	'phaazon/hop.nvim',
+	'neovim/nvim-lspconfig', -- Collection of configurations for built-in LSP client
+	'hrsh7th/nvim-cmp', -- Autocompletion plugin
+	'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+	'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+	'L3MON4D3/LuaSnip', -- Snippets plugin
 }
 require("lazy").setup(plugins)
 
