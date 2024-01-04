825a9420ae29980b6719ae091876ec9d42b9a529
Hector PHARAM <sesa439607@se.com>
2024-01-04 16:41:13 +0100

    install:Removed LSP server related commands

--- autoinstall.sh
+++ autoinstall.sh
@@ -22,17 +22,6 @@ ln -s .config/.tmux.conf .tmux.conf
 #Installing tmux manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #################################################################################
-#installing CCLS LSP server for nvim
-#dependencies
-sudo apt-get install -y zlib1g-dev libncurses-dev clang libclang-dev
-git clone --depth=1 --recursive https://github.com/MaskRay/ccls ~/ccls
-cd ~/ccls
-#clang is needed
-wget -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-cmake -H. -GNinja -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
-sudo cmake --build Release --target install
-#################################################################################
 #install fd (find alternative)
 sudo cargo install fd-find
 #################################################################################
